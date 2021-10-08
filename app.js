6119007f511562e84abb39aa5e9205fc2e8a59f2
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2021-10-08 07:34:55 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+// var arr = []
+// console.log(arr)
+
+// var arr = ["Abdul " ,"Rehman"]
+//  console.log(arr)
+
+// var arr = [1,2]
+// console.log(arr)
+
+// var arr = ["Abdul " ,"Rehman",9,86]
+// console.log(arr)
+
+// var arrName = ["Michel","Jhon","Ton"]
+// var totalMark = 500;
+// var obtMark = +prompt("Enter your Obtain Mark");
+// var percentage = obtMark/totalMark*100;
+// if(obtMark<450==0){
+//     console.log("Score Of",arrName[0], obtMark , "Percentage is" ,percentage)
+// }
+// else if(obtMark<400==0){
+//     console.log("Score of ",arrName["1"], obtMark , "Percentage is" ,percentage)
+// }
+// else if(obtMark<300==0){
+//     console.log("Score of ",arrName["2"],obtMark , "Percentage is" ,percentage)
+// }
+// else{alert("tryagain")}
+
+// var arr = [285,567,345,654]
+// var arr1 = [120,130,230,876]
+// console.log("Scores of student"+arr)
+// console.log("Order Scores of student"+arr1)
+
+// var cities = ["Karachi" , "ISlamabad" , "Lahore" ,"Quetta" ,"Peshawar"]
+
+// console.log(cities)
+
+
+// var arr = ["This" +" is " +  "my" + " cat"];
+// console.log(arr)
+// var arr= ["This is my cat"]
+// console.log(arr)
+
+// var arr = ["Keyborad","Mouse","printer","Moniter"]
+// arr.splice( "out "+1)
+// arr.splice( "out "+2)
+// arr.splice( "out "+3)
+// arr.splice( "out "+4)
+
+// console.log(arr)
+
+var arr = ["Apple","Samsung", "Motorola"," Nokia", "Sony" , "Haier"]
+document.write(arr)
